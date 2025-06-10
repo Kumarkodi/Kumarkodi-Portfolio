940acedb9132968fa5716a45365214d515fde341
Kumarkodi <Kumarkodi2001@gmail.com>
2025-06-10 15:12:18 +0530

    Resume_Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1A5fknOFCEfDU2nu-uxKISNEdZNsqaDD_/view?usp=sharing"
+            href="https://drive.google.com/file/d/1MKkUgvUPE6SY5ve6lPpoJI5qLfT80Ul-/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
